e6b6d246cc26f3c6c4a5a9c62e5c0079c584d061
Kenton Wang <smartepsh@gmail.com>
2022-03-23 15:04:38 +0800

    feat: Resource.delete/2 for delay deletion

--- test/qiniu/resource_test.exs
+++ test/qiniu/resource_test.exs
@@ -7,63 +7,93 @@ defmodule Qiniu.ResourceTest do
   import Mock
 
   test "stat" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com/stat/Yjpr", "") -> "response" end] do
+    with_mock HTTP, auth_post: fn "http://rs.qiniu.com/stat/Yjpr", "" -> "response" end do
       assert Resource.stat("b:k") == "response"
     end
   end
 
   test "copy" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com/copy/Yjpr/YjE6azE=", "") -> "response" end] do
+    with_mock HTTP, auth_post: fn "http://rs.qiniu.com/copy/Yjpr/YjE6azE=", "" -> "response" end do
       assert Resource.copy("b:k", "b1:k1") == "response"
     end
   end
 
   test "move" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com/move/Yjpr/YjE6azE=", "") -> "response" end] do
+    with_mock HTTP, auth_post: fn "http://rs.qiniu.com/move/Yjpr/YjE6azE=", "" -> "response" end do
       assert Resource.move("b:k", "b1:k1") == "response"
     end
   end
 
-  test "delete" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com/delete/Yjpr", "") -> "response" end] do
+  test "delete/1" do
+    with_mock HTTP, auth_post: fn "http://rs.qiniu.com/delete/Yjpr", "" -> "response" end do
       assert Resource.delete("b:k") == "response"
     end
   end
 
+  test "delete/2" do
+    with_mock HTTP,
+      auth_post: fn "http://rs.qiniu.com/deleteAfterDays/Yjpr/2", "" -> "response" end do
+      assert Resource.delete("b:k", 2)
+    end
+  end
+
   test "batch" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com?op=/stat/Yjpr&op=/copy/Yjpr/YjE6azE=&op=/move/Yjpr/YjE6azE=&op=/delete/Yjpr", "") -> "response" end] do
-      assert Resource.batch([[:stat, "b:k"], [:copy, "b:k", "b1:k1"], [:move, "b:k", "b1:k1"], [:delete, "b:k"]]) == "response"
+    with_mock HTTP,
+      auth_post:
+        fn "http://rs.qiniu.com?op=/stat/Yjpr&op=/copy/Yjpr/YjE6azE=&op=/move/Yjpr/YjE6azE=&op=/delete/Yjpr",
+           "" ->
+          "response"
+        end do
+      assert Resource.batch([
+               [:stat, "b:k"],
+               [:copy, "b:k", "b1:k1"],
+               [:move, "b:k", "b1:k1"],
+               [:delete, "b:k"]
+             ]) == "response"
     end
   end
 
   test "list with no options" do
-    with_mock HTTP, [auth_post: fn("http://rsf.qbox.me/list?bucket=bucket", "") -> "response" end] do
+    with_mock HTTP, auth_post: fn "http://rsf.qbox.me/list?bucket=bucket", "" -> "response" end do
       assert Resource.list("bucket") == "response"
     end
   end
 
   test "list with options" do
-    with_mock HTTP, [auth_post: fn("http://rsf.qbox.me/list?bucket=bucket&limit=10&prefix=foo&delimiter=/&marker=m", "") -> "response" end] do
-      assert Resource.list("bucket", limit: 10, prefix: "foo", delimiter: "/", marker: "m") == "response"
+    with_mock HTTP,
+      auth_post:
+        fn "http://rsf.qbox.me/list?bucket=bucket&limit=10&prefix=foo&delimiter=/&marker=m", "" ->
+          "response"
+        end do
+      assert Resource.list("bucket", limit: 10, prefix: "foo", delimiter: "/", marker: "m") ==
+               "response"
     end
   end
 
   test "fetch" do
-    with_mock HTTP, [auth_post: fn("http://iovip.qbox.me/fetch/aHR0cDovL2ltYWdlLnVybA==/to/YnVja2V0OmtleQ==", "") -> "response" end] do
+    with_mock HTTP,
+      auth_post: fn "http://iovip.qbox.me/fetch/aHR0cDovL2ltYWdlLnVybA==/to/YnVja2V0OmtleQ==",
+                    "" ->
+        "response"
+      end do
       assert Resource.fetch("http://image.url", "bucket:key") == "response"
     end
   end
 
   test "prefetch" do
-    with_mock HTTP, [auth_post: fn("http://iovip.qbox.me/prefetch/YnVja2V0OmtleQ==", "") -> "response" end] do
+    with_mock HTTP,
+      auth_post: fn "http://iovip.qbox.me/prefetch/YnVja2V0OmtleQ==", "" -> "response" end do
       assert Resource.prefetch("bucket:key") == "response"
     end
   end
 
   test "chgm" do
-    with_mock HTTP, [auth_post: fn("http://rs.qiniu.com/chgm/YnVja2V0OmtleQ==/mime/YXBwbGljYXRpb24vanNvbg==", "") -> "response" end] do
+    with_mock HTTP,
+      auth_post: fn "http://rs.qiniu.com/chgm/YnVja2V0OmtleQ==/mime/YXBwbGljYXRpb24vanNvbg==",
+                    "" ->
+        "response"
+      end do
       assert Resource.chgm("bucket:key", "application/json") == "response"
     end
   end
-
 end
